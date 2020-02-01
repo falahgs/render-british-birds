34f99b942d4f8508f68d33cdbfb28e0839a8d6bd
paul-carter <paul.carter@futurelearn.com>
2020-02-01 13:36:12 +0000

    updated for british bird image classification

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1IJayRXA_sNd4-3zpt5liWe7fP2XJ32F-'
+export_file_name = 'stage-2.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['blackbird','blue_tit','carron_crow','chaffinch','coal_tit','collared_dove','dunnock','goldcrest','goldfinch',
+ 'great_tit','greenfinch','house_sparrow','jay','long_tit','magpie','robin','starling','wood_pigeon','wren']
 path = Path(__file__).parent
 
 app = Starlette()
